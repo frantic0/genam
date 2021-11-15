fa0a9f388d83242d1ec74f80d18dafc873d238ce
Francisco Bernardo <f.bernardo@ucl.ac.uk>
2021-11-15 20:25:37 +0000

    correct process geometries with wavelength relative dimensions

--- parametric_brick.py
+++ parametric_brick.py
@@ -104,24 +104,23 @@ outerRad = 0.866
 # outerRad = 0.66
 innerRad = 0.2165 # λο / 40
 
-subLength = 2
 
 
-def parameterize_brick( barLength, barSpacing ):
+def parameterize_brick( waveLength, barLength, barSpacing ):
 
   ### Make brick sketch
   sk = geompy.Sketcher2D()
 
   # Bar height - constant 
-  OR = 0.866         # outer radius height - 
-  IR = 0.433         # inner radius height - l0/20
+  OR = waveLength/10         # outer radius height - 
+  IR = waveLength/20         # inner radius height - l0/20
   Bh = 2 * OR + IR  # bar height 
 
-  S0 = l0 - ( l0/2 + 3*barSpacing/2 + Bh/2 ) 
+  S0 = waveLength - ( waveLength/2 + 3*barSpacing/2 + Bh/2 ) 
 
   S1 = 2 * barSpacing - Bh
 
-  barTip = l0/40
+  barTip = waveLength/40
 
   sk.addPoint(0., S0) # begin from bottom left
 
@@ -154,45 +153,45 @@ def parameterize_brick( barLength, barSpacing ):
   sk.addArcRadiusAbsolute( 0.0000000, S0 + S1 + 2 * Bh, -OR, 0.0000000) # outerRadius 4
   
 
-  sk.addSegmentAbsolute( 0.0000000, l0 ) # top left corner
+  sk.addSegmentAbsolute( 0.0000000, waveLength ) # top left corner
 
 
 
-  sk.addSegmentAbsolute( l0/2, l0) # top right corner
+  sk.addSegmentAbsolute( waveLength/2, waveLength) # top right corner
 
 
   if(S0!=0):
-    sk.addSegmentAbsolute( l0/2, l0 - S0)
+    sk.addSegmentAbsolute( waveLength/2, waveLength - S0)
 
   # FLAP 3  
   
-  sk.addArcRadiusAbsolute( l0/2 - OR, l0 - S0 - OR, -OR, 0.0000000) # outerRadius 5   ----
+  sk.addArcRadiusAbsolute( waveLength/2 - OR, waveLength - S0 - OR, -OR, 0.0000000) # outerRadius 5   ----
 
-  sk.addSegmentAbsolute( l0/2 - OR - barLength, l0 - S0 - OR) # Horizontal Segment Top    ----
+  sk.addSegmentAbsolute( waveLength/2 - OR - barLength, waveLength - S0 - OR) # Horizontal Segment Top    ----
   
-  sk.addArcAbsolute( l0/2 - OR - barLength, l0 - S0 - OR - IR) # innerRadius 3            ----
+  sk.addArcAbsolute( waveLength/2 - OR - barLength, waveLength - S0 - OR - IR) # innerRadius 3            ----
 
-  sk.addSegmentAbsolute( l0/2 - OR, l0 - S0 - OR - IR) # Horizontal Segment Top       ----
+  sk.addSegmentAbsolute( waveLength/2 - OR, waveLength - S0 - OR - IR) # Horizontal Segment Top       ----
   
-  sk.addArcRadiusAbsolute( l0/2, l0 - S0 - Bh, -OR, 0.0000000) # outerRadius 6       ----
+  sk.addArcRadiusAbsolute( waveLength/2, waveLength - S0 - Bh, -OR, 0.0000000) # outerRadius 6       ----
  
 
 
-  sk.addSegmentAbsolute( l0/2, l0 - S0 - Bh - S1)  # In-between flaps vertical segment RIGHT 
+  sk.addSegmentAbsolute( waveLength/2, waveLength - S0 - Bh - S1)  # In-between flaps vertical segment RIGHT 
 
   # FLAP 4
 
-  sk.addArcRadiusAbsolute( l0/2 - OR, l0 - S0 - Bh - S1 - OR, -OR, 0.0000000) # outerRadius 7   ----
+  sk.addArcRadiusAbsolute( waveLength/2 - OR, waveLength - S0 - Bh - S1 - OR, -OR, 0.0000000) # outerRadius 7   ----
 
-  sk.addSegmentAbsolute( l0/2 - OR - barLength, l0 - S0 - Bh - S1 - OR) # Horizontal Segment Top    ----
+  sk.addSegmentAbsolute( waveLength/2 - OR - barLength, waveLength - S0 - Bh - S1 - OR) # Horizontal Segment Top    ----
   
-  sk.addArcAbsolute( l0/2 - OR - barLength, l0 - S0 - Bh - S1 - OR - IR) # innerRadius 4            ----
+  sk.addArcAbsolute( waveLength/2 - OR - barLength, waveLength - S0 - Bh - S1 - OR - IR) # innerRadius 4            ----
 
-  sk.addSegmentAbsolute( l0/2 - OR, l0 - S0 - Bh - S1 - OR - IR) # Horizontal Segment Bottom    ----
+  sk.addSegmentAbsolute( waveLength/2 - OR, waveLength - S0 - Bh - S1 - OR - IR) # Horizontal Segment Bottom    ----
   
-  sk.addArcRadiusAbsolute( l0/2, l0 - S0 - Bh - S1 - Bh, -OR, 0.0000000) # outerRadius 8
+  sk.addArcRadiusAbsolute( waveLength/2, waveLength - S0 - Bh - S1 - Bh, -OR, 0.0000000) # outerRadius 8
 
-  sk.addSegmentAbsolute( l0/2, 0.0000000) # bottom segment
+  sk.addSegmentAbsolute( waveLength/2, 0.0000000) # bottom segment
 
   sk.addSegmentAbsolute(0.0000000, 0.0000000)
   
@@ -210,7 +209,7 @@ def sketch_brick(brickID):
   barLength = list(barLen.values())[brickID - 1] * l0
   barSpacing = list(barSpa.values())[brickID -1] * l0
 
-  return parameterize_brick( barLength, barSpacing )
+  return parameterize_brick( l0, barLength, barSpacing )
 
 
 
@@ -223,7 +222,7 @@ def draw_bricks():
   for bID in range(1, 16): 
     barLength = list(barLen.values())[bID - 1] * l0   # bl * l0
     barSpacing = list(barSpa.values())[bID - 1] * l0  # bs * l0
-    Sketch = parameterize_brick( barLength, barSpacing )
+    Sketch = parameterize_brick( l0, barLength, barSpacing )
     geompy.addToStudy( Sketch, 'Sketch' + str(bID) )
 
 
--- process_geometry.py
+++ process_geometry.py
@@ -31,77 +31,168 @@ import SALOMEDS
 
 start = time.time()
 geompy = geomBuilder.New()
+
 origin = geompy.MakeVertex(0, 0, 0)
+
 x = geompy.MakeVectorDXDYDZ(1, 0, 0)
 y = geompy.MakeVectorDXDYDZ(0, 1, 0)
 z = geompy.MakeVectorDXDYDZ(0, 0, 1)
-pml_bottom = geompy.MakeBoxDXDYDZ(4.288, 4.288, 2.573)
-box_1 = geompy.MakeTranslation(geompy.MakeBoxDXDYDZ(4.288, 4.288, 4.288), 0, 0, 2.573)
-box_2 = geompy.MakeTranslation(geompy.MakeBoxDXDYDZ(4.288, 4.288, 12.776), 0, 0, 15.522)
-pml_top = geompy.MakeTranslation(pml_bottom, 0, 0, 28.298)
-brick_outer = geompy.MakeTranslation(geompy.MakeBoxDXDYDZ(4.288, 4.288, 8.661), 0, 0, 6.861)
+
+geompy.addToStudy( x, 'x' )
+geompy.addToStudy( y, 'y' )
+geompy.addToStudy( z, 'z' )
+
+waveLenght = 8.66
+
+boxSide = waveLenght/2 + 2*waveLenght/40
+
+pml_bottom = geompy.MakeBoxDXDYDZ(boxSide, boxSide, 2.573)
+
+box_1 = geompy.MakeTranslation( geompy.MakeBoxDXDYDZ( boxSide, boxSide, 4.288), 0, 0, 2.573 )
+
+box_2 = geompy.MakeTranslation( geompy.MakeBoxDXDYDZ( boxSide, boxSide, 12.776), 0, 0, 15.522 )
+
+pml_top = geompy.MakeTranslation( pml_bottom, 0, 0, 28.298 )
+
+brick_outer = geompy.MakeTranslation(geompy.MakeBoxDXDYDZ( boxSide, boxSide, 8.661), 0, 0, 6.861)
 
 
-# sk = geompy.Sketcher2D()
-# sk.addPoint(0., 1.091) # beggining from bottom left
-# sk.addArcRadiusAbsolute(0.866, 1.9570000, -0.8660000, 0.0000000) # outerRadius 1
-# sk.addSegmentAbsolute(1.3990000, 1.9570000) 
-# sk.addArcAbsolute(1.3990000, 2.3900000) # innerRadius 1
-# sk.addSegmentAbsolute(0.8660000, 2.3900000)
-# sk.addArcRadiusAbsolute(0.0000000, 3.2570000, -0.8660000, 0.0000000) # outerRadius 2
-# sk.addSegmentAbsolute(0.0000000, 3.9670000)
-# sk.addArcRadiusAbsolute(0.8660000, 4.8330000, -0.8660000, 0.0000000) # outerRadius 3
-# sk.addSegmentAbsolute(1.3990000, 4.8330000)
-# sk.addArcAbsolute(1.3990000, 5.2660000) # innerRadius 2
-# sk.addSegmentAbsolute(0.8660000, 5.2660000)
-# sk.addArcRadiusAbsolute(0.0000000, 6.1320000, -0.8660000, 0.0000000) # outerRadius 4
-# sk.addSegmentAbsolute(0.0000000, 8.6610000) # top left corner
-# sk.addSegmentAbsolute(3.8480000, 8.6610000) # top right corner
-# sk.addSegmentAbsolute(3.8480000, 7.5700000)
-# sk.addArcRadiusAbsolute(2.9810000, 6.7040000, -0.8660000, 0.0000000) # outerRadius 5
-# sk.addSegmentAbsolute(2.4480000, 6.7040000)
-# sk.addArcAbsolute(2.4480000, 6.2710000) # innerRadius 3
-# sk.addSegmentAbsolute(2.9810000, 6.2710000)
-# sk.addArcRadiusAbsolute(3.8480000, 5.4050000, -0.8660000, 0.0000000) # outerRadius 6
-# sk.addSegmentAbsolute(3.8480000, 4.6940000)
-# sk.addArcRadiusAbsolute(2.9810000, 3.8280000, -0.8660000, 0.0000000) # outerRadius 7
-# sk.addSegmentAbsolute(2.4480000, 3.8280000)
-# sk.addArcAbsolute(2.4480000, 3.3950000) # innerRadius 4
-# sk.addSegmentAbsolute(2.9810000, 3.3950000)
-# sk.addArcRadiusAbsolute(3.8480000, 2.5290000, -0.8660000, 0.0000000) # outerRadius 8
-# sk.addSegmentAbsolute(3.8480000, 0.0000000) 
-# sk.addSegmentAbsolute(0.0000000, 0.0000000)
-# sk.addSegmentAbsolute(0.0000000, 1.0910000)
-# wire = geompy.MakeMarker(0, 0, 0, 1, 0, 0, 0, 1, 0)
-# Sketch_1 = sk.wire(wire)
+
+def draw_hardcoded_brick():
+  ### Make brick sketch
+  sk = geompy.Sketcher2D()
+  sk.addPoint(0.0000000, 1.0910000)
+  sk.addArcRadiusAbsolute(0.8660000, 1.9570000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(1.3990000, 1.9570000)
+  sk.addArcAbsolute(1.3990000, 2.3900000)
+  sk.addSegmentAbsolute(0.8660000, 2.3900000)
+  sk.addArcRadiusAbsolute(0.0000000, 3.2570000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(0.0000000, 3.9670000)
+  sk.addArcRadiusAbsolute(0.8660000, 4.8330000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(1.3990000, 4.8330000)
+  sk.addArcAbsolute(1.3990000, 5.2660000)
+  sk.addSegmentAbsolute(0.8660000, 5.2660000)
+  sk.addArcRadiusAbsolute(0.0000000, 6.1320000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(0.0000000, 8.6610000)
+  sk.addSegmentAbsolute(3.8480000, 8.6610000)
+  sk.addSegmentAbsolute(3.8480000, 7.5700000)
+  sk.addArcRadiusAbsolute(2.9810000, 6.7040000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(2.4480000, 6.7040000)
+  sk.addArcAbsolute(2.4480000, 6.2710000)
+  sk.addSegmentAbsolute(2.9810000, 6.2710000)
+  sk.addArcRadiusAbsolute(3.8480000, 5.4050000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(3.8480000, 4.6940000)
+  sk.addArcRadiusAbsolute(2.9810000, 3.8280000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(2.4480000, 3.8280000)
+  sk.addArcAbsolute(2.4480000, 3.3950000)
+  sk.addSegmentAbsolute(2.9810000, 3.3950000)
+  sk.addArcRadiusAbsolute(3.8480000, 2.5290000, -0.8660000, 0.0000000)
+  sk.addSegmentAbsolute(3.8480000, 0.0000000)
+  sk.addSegmentAbsolute(0.0000000, 0.0000000)
+  sk.addSegmentAbsolute(0.0000000, 1.0910000)
+  wire = geompy.MakeMarker(0, 0, 0, 1, 0, 0, 0, 1, 0)
+  return sk.wire(wire)
+
+
+barLen = {  'b1': 0.062, 
+            'b2': 0.092, 
+            'b3': 0.112, 
+            'b4': 0.132, 
+            'b5': 0.152, 
+            'b6': 0.162, 
+            'b7': 0.171, 
+            'b8': 0.191, 
+            'b9': 0.221, 
+            'b10': 0.241, 
+            'b11': 0.251, 
+            'b12': 0.271, 
+            'b13': 0.281, 
+            'b14': 0.301, 
+            'b15': 0.321 
+          }
+
+barSpa = {  'b1': 0.216, 
+            'b2': 0.212, 
+            'b3': 0.207,
+            'b4': 0.189, 
+            'b5': 0.161, 
+            'b6': 0.166, 
+            'b7': 0.171, 
+            'b8': 0.134, 
+            'b9': 0.257, 
+            'b10': 0.234, 
+            'b11': 0.230, 
+            'b12': 0.207, 
+            'b13': 0.203, 
+            'b14': 0.175, 
+            'b15': 0.152 
+          }
+
+filletRad = { 'b1': 0.062, 
+              'b2': 0.092, 
+              'b3': 0.1,
+              'b4': 0.1, 
+              'b5': 0.1, 
+              'b6': 0.1, 
+              'b7': 0.1, 
+              'b8': 0.1, 
+              'b9': 0.1, 
+              'b10': 0.1, 
+              'b11': 0.1, 
+              'b12': 0.1, 
+              'b13': 0.1, 
+              'b14': 0.1, 
+              'b15': 0.1 
+          }
+
+
+
+brickID = 1
+barLength = list(barLen.values())[brickID - 1] * waveLenght
+barSpacing = list(barSpa.values())[brickID -1] * waveLenght
+
+# Sketch_1 = sketch_brick(6)
+Sketch_1 = parameterize_brick( waveLenght, barLength, barSpacing )
+
 # geompy.addToStudy( Sketch_1, 'Sketch' )
-
-
-Sketch_1 = sketch_brick(6)
-
-l0 = 8.66
 
 ### Convert sketch to 3d object
 rotation = [(x, 90)]
-translation = (0.22, 4.068, 6.861)
+translation = ( waveLenght/40, waveLenght/40 + waveLenght/2, 6.861)
 
-brick_inner = sketch_to_volume( geompy, Sketch_1, 3.848, rotation, translation)
+brick_inner = sketch_to_volume( geompy, Sketch_1, waveLenght/2, rotation, translation)
 
-# brick_fused = geompy.MakeFuseList([brick_outer, brick_inner], True, True)
-# brick = geompy.MakeCutList(brick_fused, [brick_inner], True)
-# air = geompy.MakeFuseList([box_1, box_2, brick_inner], True, True)
-# Structure = geompy.MakePartition([pml_bottom, pml_top, brick, air], [], [], [], geompy.ShapeType["SOLID"], 0, [], 0)
+
+# geompy.addToStudy( brick_inner, 'Sketch' )
+# geompy.addToStudy(  draw_hardcoded_brick(outerRadius), 'Sketch' )
+
+
+brick_fused = geompy.MakeFuseList([brick_outer, brick_inner], True, True)
+geompy.addToStudy(  brick_fused, 'Fused' )
+
+brick = geompy.MakeCutList(brick_fused, [brick_inner], True)
+geompy.addToStudy(  brick, 'Brick' )
+
+air = geompy.MakeFuseList([box_1, box_2, brick_inner], True, True)
+geompy.addToStudy(  air, 'Air' )
+
+Structure = geompy.MakePartition([pml_bottom, pml_top, brick, air], [], [], [], geompy.ShapeType["SOLID"], 0, [], 0)
+geompy.addToStudy(  Structure, 'Structure' )
 
 # solids = [Solid_1, Solid_2, Solid_3, Solid_4] = geompy.ExtractShapes(Structure, geompy.ShapeType["SOLID"], True)
-# # solids = geompy.ExtractShapes(Structure, geompy.ShapeType["SOLID"], True)
+# solids = [Solid_1, Solid_2, Solid_3, Solid_4, Solid_5] = geompy.ExtractShapes(Structure, geompy.ShapeType["SOLID"], True)
+solids = geompy.ExtractShapes(Structure, geompy.ShapeType["SOLID"], True)
 
-# # print(solids)
+print(solids)
 
-# # faces = [Face_1, Face_2, Face_3, Face_4, Face_5, Face_6, Face_7, Face_8, Face_9, Face_10, Face_11, Face_12,\
-# #         Face_13, Face_14, Face_15, Face_16, Face_17, Face_18, Face_19, Face_20, Face_21, Face_22, Face_23, \
-# #         Face_24, Face_25, Face_26, Face_27, Face_28, Face_29, Face_30, Face_31, Face_32, Face_33, Face_34, \
-# #         Face_35, Face_36, Face_37, Face_38, Face_39, Face_40, Face_41, Face_42, Face_43, Face_44, Face_45, \
-# #         Face_46, Face_47, Face_48, Face_49, Face_50, Face_51, Face_52, Face_53, Face_54] = geompy.ExtractShapes(Structure, geompy.ShapeType["FACE"], True)
+# faces = [Face_1, Face_2, Face_3, Face_4, Face_5, Face_6, Face_7, Face_8, Face_9, Face_10, Face_11, Face_12,\
+#         Face_13, Face_14, Face_15, Face_16, Face_17, Face_18, Face_19, Face_20, Face_21, Face_22, Face_23, \
+#         Face_24, Face_25, Face_26, Face_27, Face_28, Face_29, Face_30, Face_31, Face_32, Face_33, Face_34, \
+#         Face_35, Face_36, Face_37, Face_38, Face_39, Face_40, Face_41, Face_42, Face_43, Face_44, Face_45, \
+#         Face_46, Face_47, Face_48, Face_49, Face_50, Face_51, Face_52, Face_53, Face_54] = geompy.ExtractShapes(Structure, geompy.ShapeType["FACE"], True)
+
+# faces = geompy.ExtractShapes(Structure, geompy.ShapeType["FACE"], True) # generates 67 faces instead of 54
+# print(len(faces))
 
 # # Autogroups in geometry for meshing
 # Auto_group_for_top_bottom_walls = geompy.CreateGroup(Structure, geompy.ShapeType["FACE"]) # set top & bottom walls
