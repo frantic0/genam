4eb06dbc88fb772210b296d442d15e08a78685ec
Francisco Bernardo <f.bernardo@ucl.ac.uk>
2022-04-27 22:38:11 +0100

    simplifying focus lens calculation

--- genam/focus_calc.py
+++ genam/focus_calc.py
@@ -13,29 +13,40 @@ def calculate_focus_lens_phase_distribution(m, n, focal_length = 0.1):
     wavelength = c/f # m
     k = 2*np.pi/wavelength # wave number (rads/m)
 
-    # evpsl = ( elem_num/4 ) * wavelength # evaluation plane side length
     plane_length = lambda x : wavelength * x / 4 
 
-    dx = wavelength/2 # [m]
+    dx = wavelength / 2 # [m]
 
     aperture_size = ( int( 2* plane_length(m) /dx ), int( 2* plane_length(n) /dx ) ) # aperture size (number of unit cells) 
+    print(aperture_size)
 
-    x_vec = np.arange(-( aperture_size[0]/2 - 0.5 ) * dx, ( aperture_size[0]/2 + 0.5) * dx, dx)
-    y_vec = np.arange(-( aperture_size[1]/2 - 0.5 ) * dx, ( aperture_size[1]/2 + 0.5) * dx, dx)
+    x_vec = np.arange( -( aperture_size[0]/2 - 0.5 ) * dx, 
+                        ( aperture_size[0]/2 + 0.5 ) * dx, 
+                        dx 
+                    )
+    
+    y_vec = np.arange( 
+                        -( aperture_size[1]/2 - 0.5 ) * dx, 
+                         ( aperture_size[1]/2 + 0.5 ) * dx, 
+                         dx 
+                    )
 
     xx, yy = np.meshgrid(x_vec, y_vec)
+    print(xx)
+    print(yy)
+    
+    travel_distances = np.sqrt( xx**2 + yy**2 + focal_length**2 ) # matrix of distances from centre of each elem to focus
+    print(travel_distances)
 
-    travel_distance_array = np.sqrt( xx**2 + yy**2 + focal_length**2 ) # matrix of distances from centre of each elem to focus
+    total_phases = -travel_distances * k # total change in phase of waves as they travel this distance.
 
-    total_phase_array = -travel_distance_array * k # total change in phase of waves as they travel this distance.
-
-    return np.remainder(total_phase_array, 2*np.pi) # normalise phase to 0-2π [rads] to perform focusing, assuming incident plane wave
+    return np.remainder(total_phases, 2*np.pi) # normalise phase to 0-2π [rads] to perform focusing, assuming incident plane wave
 
 
 
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7,7))
 
-im = ax.imshow( calculate_focus_lens_phase_distribution(16, 4, 0.1) )
+im = ax.imshow( calculate_focus_lens_phase_distribution(8, 2, 0.1) )
 
 plt.colorbar(im)
 plt.show()
