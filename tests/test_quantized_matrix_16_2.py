de026b8767198504f26e57e86672781b49a01315
Francisco Bernardo <f.bernardo@ucl.ac.uk>
2022-05-25 16:18:21 +0100

    uncommented time metrics on serial test 16 2

--- tests/test_quantized_matrix_16_2.py
+++ tests/test_quantized_matrix_16_2.py
@@ -30,23 +30,23 @@ lens_config = lens_configurator( quantized_matrix_16_2 )
 
 lens_name = 'quantized_matrix_16_2' 
 
-# # # Create lens with name, bricks ID and mesh configurations 
+# Create lens with name, bricks ID and mesh configurations 
 
 lens = Lens( lens_config, mesh_configurator(3), name = lens_name  )
 
-# # start = time.time()
+start = time.time()
 
 lens.process_geometry() # Create the lens geometry 
 
-# # print("Geometry computation time: {:.2f} sec".format(time.time() - start) )
+print("Geometry computation time: {:.2f} sec".format(time.time() - start) )
 
-# # start = time.time()
+start = time.time()
 
 lens.process_mesh() # Create lens mesh 
 
-# # print("Mesh computation time: {:.2f} sec".format( time.time() - start) )
+print("Mesh computation time: {:.2f} sec".format( time.time() - start) )
 
-# # start = time.time()
+start = time.time()
 
 
 DATASET_PATH = Path('/SAN/uclic/ammdgop/data')              # Dataset path, where all data will be stored - .unv mesh files and solver directories
@@ -55,15 +55,15 @@ SIF_PATH = Path('test_quantised_matrix.sif')
 SOLVER_DATA_PATH = DATASET_PATH.joinpath( lens_name )       #  solver *.mesh files, sif. file
 
 lens.export_mesh( str( UNV_PATH ) ) # export .unv mesh file, requires conversion to string
-# print("Mesh exported to Elmer format: {:.2f} sec".format( time.time() - start) )
+print("Mesh exported to Elmer format: {:.2f} sec".format( time.time() - start) )
 
-# start = time.time()
+start = time.time()
 
 convert_mesh( UNV_PATH ) # run elmergrid convert .unv mesh file to elmer format *.mesh files in a directory 
 copy_solver_templates( SOLVER_DATA_PATH )          # copy all the necessary templates to run elmer solver
 copy_sif( SOLVER_DATA_PATH, SIF_PATH )          # copy solver input file
 
-# print("Elmer template copied: {:.2f} sec".format( time.time() - start) )
+print("Elmer template copied: {:.2f} sec".format( time.time() - start) )
 
 run_elmer_solver( SOLVER_DATA_PATH )
 
