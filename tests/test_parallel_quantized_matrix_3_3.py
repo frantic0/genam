b4c348ce0e5408cf3309b93feed1095ed7a50eaf
Francisco Bernardo <f.bernardo@ucl.ac.uk>
2022-06-23 18:44:52 +0100

    added time points to 3x3 and test new subprocess call - successful

--- tests/test_parallel_quantized_matrix_3_3.py
+++ tests/test_parallel_quantized_matrix_3_3.py
@@ -17,7 +17,7 @@ sys.path.insert(0, r'C:/Users/Francisco/Documents/dev/pipeline/tests')
 
 
 # Genam Lens, mesh configurator
-from matrices.quantized_2_2 import quantized_matrix_2_2
+from matrices.quantized_3_3 import quantized_matrix_3_3
 from genam.lens import Lens
 from genam.configuration.lens import configurator as lens_configurator 
 from genam.configuration.mesh import configurator as mesh_configurator
@@ -25,47 +25,41 @@ from genam.solver import convert_mesh, copy_solver_templates, copy_sif, run_elme
 from genam.analysis import Analysis
 
 
-
-lens_config = lens_configurator( quantized_matrix_2_2 )
-
-lens_name = 'quantized_matrix_2_2' 
-
-# # # Create lens with name, bricks ID and mesh configurations 
-
-lens = Lens( lens_config, mesh_configurator(3), name = lens_name  )
-
-# # start = time.time()
-
-lens.process_geometry() # Create the lens geometry 
-
-# # print("Geometry computation time: {:.2f} sec".format(time.time() - start) )
-
-# # start = time.time()
-
-lens.process_mesh() # Create lens mesh 
-
-# # print("Mesh computation time: {:.2f} sec".format( time.time() - start) )
-
-# # start = time.time()
-
+lens_name = 'quantized_matrix_3_3' 
 
 DATASET_PATH = Path('/SAN/uclic/ammdgop/data')              # Dataset path, where all data will be stored - .unv mesh files and solver directories
 UNV_PATH = DATASET_PATH.joinpath( lens_name + '.unv')       
 SIF_PATH = Path('test_quantised_matrix.sif')       
 SOLVER_DATA_PATH = DATASET_PATH.joinpath( lens_name )       #  solver *.mesh files, sif. file
 
+
+# Create lens with name, bricks ID and mesh configurations 
+lens_config = lens_configurator( quantized_matrix_3_3 )
+lens = Lens( lens_config, mesh_configurator(1), name = lens_name  )
+
+total_start = start = time.time()
+lens.process_geometry() # Create the lens geometry 
+print("Geometry computation time: {:.2f} sec".format(time.time() - start) )
+
+start = time.time()
+lens.process_mesh() # Create lens mesh 
+print("Mesh computation time: {:.2f} sec".format( time.time() - start) )
+
+start = time.time()
 lens.export_mesh( str( UNV_PATH ) ) # export .unv mesh file, requires conversion to string
-# print("Mesh exported to Elmer format: {:.2f} sec".format( time.time() - start) )
+print("Mesh exported to Elmer format: {:.2f} sec".format( time.time() - start) )
 
-# start = time.time()
 
+start = time.time()
 convert_mesh( UNV_PATH ) # run elmergrid convert .unv mesh file to elmer format *.mesh files in a directory 
 copy_solver_templates( SOLVER_DATA_PATH )          # copy all the necessary templates to run elmer solver
 copy_sif( SOLVER_DATA_PATH, SIF_PATH )          # copy solver input file
+print("Elmer template copied: {:.2f} sec".format( time.time() - start) )
 
-# print("Elmer template copied: {:.2f} sec".format( time.time() - start) )
 
+start = time.time()
 run_elmer_solver( SOLVER_DATA_PATH )
+print("Elmer simulation completed: {:.2f} sec".format( time.time() - start) )
 
 analysis = Analysis( str(SOLVER_DATA_PATH.joinpath( 'case-40000_t0001.vtu' )) )
 
@@ -77,4 +71,5 @@ optimisation_target_id = analysis.points.index(optimisation_targets[len(optimisa
 optimisation_target_pressure = analysis._absolute_pressure.GetValue(optimisation_target_id)
 
 print("Optimisation target: {} {} {} ".format( optimisation_target_id, optimisation_target, optimisation_target_pressure ) )
+print("Total pipeline completed: {:.2f} sec".format( time.time() - total_start) )
 
