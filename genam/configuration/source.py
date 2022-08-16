f22f054dcab59475980a4f0e46bc6017d5592f47
Francisco Bernardo <f.bernardo@ucl.ac.uk>
2022-08-16 04:50:11 +0100

    source configurator rewritten as functional

--- genam/configuration/source.py
+++ genam/configuration/source.py
@@ -1,11 +1,6 @@
-import numpy as np, matplotlib.pyplot as plt, matplotlib.colors, os, sys
+import numpy as np, matplotlib.pyplot as plt, matplotlib.colors, matplotlib.cm as cm, os, sys
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
-# home_path = os.path.abspath(os.getcwd()+"/..")
-# print(home_path)
-# sys.path.append(home_path)
-# sys.path.append(home_path+"/JamesAHardwick")
-# from constrained_GS_functions import *
-# from functions import *
+
 
 
 # ---> physics params <---
@@ -20,8 +15,6 @@ dist = .1 # focal length of the board [m] and distance to the AMM
 prop_dist = 12*lam # focal length of the AMM [m]
 resolution = 10
 iterations = 200
-
-m, n = 2,2 # Metasurface size (in units of dx_AMM)
 
 
 # Transducer piston model
@@ -49,28 +42,111 @@ def piston_model_matrix(rxy, rxyz, k, p0=8.02, d=10.5/1000):
     # return 2*(p0/b)*(tay/rxyz)*np.exp(1j*k*rxyz)
 
 
-def pesb(side_length_x, side_length_y, dist, cell_spacing, tx, ty, tz):
+def inlet_grid( unit_cell_size,
+                side_length_x, 
+                side_length_y ):
 
-    """pressure_evaluation_space_builder"""
+    ex = np.arange(   # x side length vector 
+        unit_cell_size * (-side_length_x + 1)/2, 
+        unit_cell_size * ( side_length_x + 1)/2, 
+        unit_cell_size ) 
     
-    ex = np.arange(-side_length_x/2 + cell_spacing/2, side_length_x/2 + cell_spacing/2, cell_spacing) # x side length vector 
-    ey = np.arange(-side_length_y/2 + cell_spacing/2, side_length_y/2 + cell_spacing/2, cell_spacing) # y side length vector
+
+    ey = np.arange(   # y side length vector
+        unit_cell_size * (-side_length_y + 1)/2, 
+        unit_cell_size * ( side_length_y + 1)/2, 
+        unit_cell_size )
     
     exx, eyy = np.meshgrid(ex, ey)
-    # x, y & z vectors for evaluation-plane sample points:
-    px, py, pz = exx.flatten(), eyy.flatten(), dist*np.ones_like(exx.flatten())
 
+    return exx, eyy
+
+
+def transducer_grid( pitch, # inter-element spacing or pitch between adjacent transducer
+                     transducers_m, 
+                     transducers_n ):
+
+    tx = np.arange(          # transducer arrangement in x 
+        pitch * (-transducers_m + 1 ) /2, 
+        pitch * ( transducers_m + 1 ) /2, 
+        pitch ) 
+
+    ty = np.arange(          # transducer arrangement in y 
+        pitch * (-transducers_n + 1 ) /2, 
+        pitch * ( transducers_n + 1 ) /2, 
+        pitch ) 
     
-    # Grids to describe the vector distances between each transducer & evaluation plane sample point.
-    txv, pxv = np.meshgrid(tx,px)
-    tyv, pyv = np.meshgrid(ty,py)
-    tzv, pzv = np.meshgrid(tz,pz)
+    txx, tyy = np.meshgrid(tx, ty)
+    tzz = np.zeros_like(txx)  # transducer arrangement in z 
+
+    return txx, tyy, tzz
+
+
+def transducer_inlet_grid(  transducer_grid,
+                            inlet_grid,
+                            dist ):
+
+    txx, tyy, tzz = transducer_grid
+
+    exx, eyy = inlet_grid
+
+    # x, y & z vectors for evaluation-plane sample points:
+    px, py = exx.flatten(), eyy.flatten(),
+    pz = dist * np.ones_like( exx.flatten() )
     
-    rxyz = np.sqrt((txv-pxv)**2 + (tyv-pyv)**2 + (tzv-pzv)**2)  # Pythagoras for xyz distances
-    rxy = np.sqrt((txv-pxv)**2 + (tyv-pyv)**2)                  # Pythagoras for xy distances
+    txv, pxv = np.meshgrid( txx, px )
+    tyv, pyv = np.meshgrid( tyy, py )
+    tzv, pzv = np.meshgrid( tzz, pz )                     
+
+    rxyz = np.sqrt( (txv-pxv)**2 + (tyv-pyv)**2 + (tzv-pzv)**2 )  # Pythagoras for xyz distances
+    rxy  = np.sqrt( (txv-pxv)**2 + (tyv-pyv)**2 )                 # Pythagoras for xy distances
     
     return rxyz, rxy
 
+def plotter( Pf ):
+
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20, 7))
+
+    im1 = ax1.imshow(   # phase
+            np.mod( np.angle(Pf), 2*np.pi), 
+            cmap = cm.get_cmap('viridis'), 
+            vmin = 0, 
+            vmax = 2*np.pi ) 
+
+    divider = make_axes_locatable(ax1)
+    cax1 = divider.append_axes("right", size='5%', pad=.1)
+
+    # ax1.title.set_text("phasemap")
+    # ax1.set_title("phasemap", fontsize=15)
+    plt.colorbar(im1, cax=cax1)
+
+    im2 = ax2.imshow(   
+            abs(Pf), 
+            cmap=cm.get_cmap('jet') )
+
+    divider = make_axes_locatable(ax2)
+    cax2 = divider.append_axes("right", size='5%', pad=.1)
+    
+    ax2.set_title("propagation", fontsize=15)
+    
+    plt.colorbar(im2, cax=cax2)
+
+    fig.tight_layout()
+
+    plt.show()
+
+def write_complex_pressure_inlet_( configurator,
+
+                                  path ):
+
+
+    exx, eyy,  Pf =  configurator
+
+    for m in m = 2,
+                    n = 2,  
+
+
+    pass
 
 def configurator(   dist,
                     tm = 1,
@@ -78,68 +154,30 @@ def configurator(   dist,
                     m = 2,
                     n = 2,  
                     p0 = 8.02,                  # tranducer reference pressure [Pa] 
-                    dx_tran = 10.5/1000,        # diameter of a transducer [m] (actually they are 10mm in diameter, with a spacing of 0.5mm)
-                    ):
+                    pitch = 10.5/1000 ):        # inter-element spacing or pitch between adjacent transducer [m] 
+                                                # ( diameter of a transducer 10mm + spacing of 0.5mm )
 
     A = 1                                       # amplitude for each transducer to take (max=1, min=0)
 
-    x = np.arange(  ( dx_tran/2 ) * ( 1 - tm ),   # transducer arrangement in x
-                    ( dx_tran/2 ) * ( 1 + tm ), 
-                    dx_tran ) 
+    rxyz, rxy = transducer_inlet_grid( 
+                    transducer_grid( pitch, tm, tn ),
+                    inlet_grid( lam/2, m, n ), 
+                    dist
+                    )
 
-    y = np.arange(  ( dx_tran/2 ) * ( 1 - tn ),
-                    ( dx_tran/2 ) * ( 1 + tn ),
-                    dx_tran ) # transducer arrangement in y
-
-    xx, yy = np.meshgrid(x, y)
-
-    zz = np.zeros_like(xx) # transducer arrangement in z
-
-    rxyz, rxy = pesb(   m * lam/2, n * lam/2, 
-                        dist, 
-                        lam/2, 
-                        xx, yy, zz )
-
-    H = piston_model_matrix( rxy, rxyz, k, p0, dx_tran ) # propagator
+    H = piston_model_matrix( rxy, rxyz, k, p0, pitch ) # propagator
 
     Pt = A * np.ones( tm * tn ) * np.exp( 1j * np.zeros( tm * tn ) ) # transducer complex pressure
 
     Pf = np.dot( H, Pt ).reshape( m, n ) # propagate to far field and reshape to array
 
+    # print("H:", H.shape)
+    # print("Pt:", Pt)
+    # print("Pt shape: ", Pt.shape)
+    # print(f'Pf: {Pf}')
+    # print(f'Pf shape: {Pf.shape}')
 
-    print("H:", H.shape)
-    print("Pt:", Pt)
-    print("Pt shape: ", Pt.shape)
-    print(f'Pf: {Pf}')
-    print(f'Pf shape: {Pf.shape}')
+    exx, eyy = inlet_grid( lam/2, m, n )
 
-    # Plotting
-    # fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20, 7))
-
-    # im1 = ax1.imshow(   np.mod( np.angle(Pf), 2*np.pi), 
-    #                     cmap= cm.get_cmap('viridis'), 
-    #                     vmin=0, 
-    #                     vmax=2*np.pi ) # phase
-
-    # divider = make_axes_locatable(ax1)
-    # cax1 = divider.append_axes("right", size='5%', pad=.1)
-
-    # # ax1.title.set_text("phasemap")
-    # # ax1.set_title("phasemap", fontsize=15)
-    # plt.colorbar(im1, cax=cax1)
-
-    # im2 = ax2.imshow(   abs(Pf), 
-    #                     cmap=cm.get_cmap('jet') )
-
-    # divider = make_axes_locatable(ax2)
-    # cax2 = divider.append_axes("right", size='5%', pad=.1)
-    
-    # ax2.set_title("propagation", fontsize=15)
-    
-    # plt.colorbar(im2, cax=cax2)
-
-    # fig.tight_layout()
-
-    # plt.show()
-
-    pass
+    return exx.flatten(), eyy.flatten(), Pf.flatten()
+   
